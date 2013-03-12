3084dcba18ff04d32a2a660e3447a2afbaa127df
dedrain <ticketsforfenriz@gmail.com>
2013-03-12 12:42:29 +0400

    Тестовые проекты для проверки функционала, после окончания тестирования, удалить

---
+++ PluginsCore/ExternalPlugin/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PluginsCore.MEFComponents;
+using PluginsCore;
+using System.ComponentModel.Composition;
+using System.Reflection;
+using ExternalPlugin;
+
+namespace ExternalPlugin
+{
+    public interface ILogger : IPlugin
+    {
+        string LogFileName { get; set; }
+    }
+
+    [IPluginExport(typeof(IPlugin))]
+    public class Logger : ILogger
+    {
+        private bool __init_CurrentAssembly;
+        private Assembly _CurrentAssembly;
+        public Assembly CurrentAssembly
+        {
+            get
+            {
+                if (!__init_CurrentAssembly)
+                {
+                    _CurrentAssembly = Assembly.GetAssembly(this.GetType());
+                    __init_CurrentAssembly = true;
+                }
+                return _CurrentAssembly;
+            }
+        }
+
+        /// <summary>
+        /// Тип плагина
+        /// </summary>
+        public PluginType PluginType
+        {
+            get;
+            set;
+        }
+
+        public object ThroughData
+        {
+            get;
+            set;
+        }
+
+        private string _LogFileName = "test";
+        public string LogFileName
+        {
+            get { return _LogFileName; }
+            set { _LogFileName = value; }
+        }
+
+        public Logger()
+        {
+        }
+
+        public void Initialize()
+        {
+        }
+    }
+}
---
+++ PluginsCore/UnitTesting/CommonTestCore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PluginsCore;
+using System.ComponentModel.Composition.Hosting;
+using PluginsCore.MEFComponents;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class CommonTestCore
+    {
+        [IPluginImport(typeof(IPlugin))]
+        IPlugin Logger;
+
+        [TestMethod]
+        public void TestCore()
+        {
+            PluginsContainer container = new PluginsContainer();
+            foreach (Lazy<IPlugin> item in container.Plugins)
+            {
+                Type type = item.Value.GetType();
+                dynamic logger = Activator.CreateInstance(type);
+                string test = logger.LogFileName;
+                logger.Initialize();
+            }
+            
+        }
+    }
+}
